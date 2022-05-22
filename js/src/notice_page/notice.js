c74fb6aa93e94b403d3074fdb474c451d55ffa85
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-05-22 20:56:49 +0900

    notice modal 창 구현

--- js/src/notice_page/notice.js
+++ js/src/notice_page/notice.js
@@ -10,6 +10,7 @@ var jsonData = '../data/notice_data.json';
 $.getJSON(jsonData, function(data){
 var noticeData = data.reverse();
 
+// step 1. data 불러와서 내용 삽입
 // 변수
 var noticeBox = $('#noticeBox');
 var noticeTable = noticeBox.find('.notice_table');
@@ -39,12 +40,43 @@ for (; i < dataLen; i++){
 
 tr = tableBody.children('tr');
 
-tr.on('click', function(){
-  var j = $(this).index();
-  dataN = noticeData[j];
-  dataLink = "../page/notice/"+dataN.link;
+// step 2. tr 클릭했을 때 해당 모달 창 띄우기
+// 변수
+var modalWindow = noticeBox.find('.notice_modal_window');
+var modalData = modalWindow.find('.modal_data');
+var closeBtn = modalData.find('.close_btn');
+var modalContent = modalData.find('.modal_content');
+var modalContentCode = '<div class="modal_title"><h3 class="n_title"></h3><p class="n_writer"></p></div><div class="modal_con"><img src="../../img/notice/notice_01.jpg" alt="notice 01"></div>'
+var modalTitle, modalWriter, modalImg, modalAlt;
 
-  window.location.href = dataLink;
+modalWindow.hide();
+
+tr.on('click', function(){
+  modalContent.empty();
+  modalContent.append(modalContentCode);
+  var j = $(this).index();
+
+  var modalN = modalContent;
+  modalTitle = modalN.find('.n_title');
+  modalWriter = modalN.find('.n_writer');
+  modalImg = modalN.find('img');
+  modalUrl = "../../img/notice/";
+  modalAlt = modalN.find('img').attr('alt');
+
+  dataN = noticeData[j];
+
+  modalTitle.text(dataN.title);
+  modalWriter.text(dataN.writer);
+  modalImg.attr('src', modalUrl+dataN.content);
+  modalImg.attr('alt', dataN.id);
+
+  modalWindow.fadeIn();
+  
+});
+
+closeBtn.on('click', function(e){
+  e.preventDefault();
+  modalWindow.fadeOut();
 })
 
 })
