25987011f1f5bf7fc2a2dd2743ae564815be680b
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-05-22 20:13:41 +0900

    Create import_data.js

---
+++ js/src/notice_page/import_data.js
@@ -0,0 +1,33 @@
+// import_data.js
+(function($){
+setTimeout(function(){
+  var deviceCk = $.CheckType;
+  console.log(deviceCk);
+  
+  // 공통영역 불러오기 ----------------------------------
+  // 변수
+  var body = $('body');
+  var headBox = $('#headBox');
+  var footBox = $('#footBox');
+  var baseUrl = "../page/common/";
+  var importPage = ['headBox.html', 'footBox.html'];
+
+  // 기능 수행
+  headBox.load(baseUrl + importPage[0], function(){
+    var headMenu = '<script src="../js/src/commonFile/headBox_handhelds.js"></script>';
+    (deviceCk === 'smartphone' || deviceCk === 'tablet') ? body.append(headMenu) : body.remove($('.head_script'))
+  });
+
+  footBox.load(baseUrl + importPage[1]);
+
+  // noticeBox 내용 불러오기
+  var noticeBox = $('#noticeBox');
+  var noticeUrl = "../page/notice/noticeBox.html";
+
+  noticeBox.load(noticeUrl, function(){
+    var noticeJs = '<script src="../js/src/notice_page/notice.js"></script>';
+    body.append(noticeJs);
+  });
+},100)
+
+})(jQuery);
